eb5da6502a8f7043283022e1d090f2176906f299
PizzaY0L0 <82245001+PizzaY0L0@users.noreply.github.com>
2024-09-16 22:50:50 -0300

    gp3

--- test/test.py
+++ test/test.py
@@ -5,7 +5,6 @@ from unittest.mock import patch
 
 class TestGeradorDeSenhas(TestCase):
 
-    # Testando se a função de geração de senhas está funcionando corretamente
     def test_generate_password(self):
         password = generate_password(10)
         self.assertEqual(len(password), 10)
@@ -13,23 +12,22 @@ class TestGeradorDeSenhas(TestCase):
         self.assertTrue(any(char in string.digits for char in password))
         self.assertTrue(any(char in string.punctuation for char in password))
 
-    # Testando a função principal simulando entradas do usuário
-    @patch('builtins.input', return_value='10')  # Mock para o input
-    @patch('builtins.print')  # Mock para o print
+    @patch('builtins.input', return_value='10')  # Simula o input do usuário
+    @patch('builtins.print')  # Simula o print
     def test_gerador_de_senhas_valid_input(self, mock_print, mock_input):
         senha = gerador_de_senhas()
         mock_input.assert_called_once_with("Digite o comprimento desejado da senha (número de caracteres): ")
-        self.assertEqual(len(senha), 10)  # Verifica se a senha gerada tem 10 caracteres
+        self.assertEqual(len(senha), 10)
         mock_print.assert_any_call("Bem-vindo ao Gerador de Senhas!")
         mock_print.assert_any_call(f"A sua senha gerada é: {senha}")
 
-    @patch('builtins.input', return_value='-5')
+    @patch('builtins.input', return_value='-5')  # Simula input de comprimento negativo
     @patch('builtins.print')
     def test_gerador_de_senhas_negative_length(self, mock_print, mock_input):
         gerador_de_senhas()
         mock_print.assert_called_with("O comprimento deve ser um número positivo.")
 
-    @patch('builtins.input', return_value='abc')
+    @patch('builtins.input', return_value='abc')  # Simula input inválido
     @patch('builtins.print')
     def test_gerador_de_senhas_invalid_input(self, mock_print, mock_input):
         gerador_de_senhas()
